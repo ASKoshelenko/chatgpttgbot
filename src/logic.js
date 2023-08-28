9ef8b86a3ec42d7e220db2e716483038f83ec9b2
Alexey Koshelenko <39233929+ASKoshelenko@users.noreply.github.com>
2023-08-28 12:54:55 +0300

    Fix some errors
    Errors with API connect and voice messages

--- src/logic.js
+++ src/logic.js
@@ -5,8 +5,8 @@ export const INITIAL_SESSION = {
 }
 
 export async function initCommand(ctx) {
-  ctx.session = INITIAL_SESSION
-  await ctx.reply('Waiting for your voice or text message')
+  ctx.session = { ...INITIAL_SESSION }
+  await ctx.reply('Жду вашего голосового или текстового сообщения')
 }
 
 export async function processTextToChat(ctx, content) {
